e1f9fa2064630a930612ca7f4b24c78ea71a057e
Majid Ziaei <ziaee.majid@gmail.com>
2023-05-27 08:03:25 +0330

    save

--- src/application/base/ability/ability.factory.spec.ts
+++ src/application/base/ability/ability.factory.spec.ts
@@ -6,27 +6,34 @@ import { Acl } from 'src/infrastructure/database/entities/Acl';
 import { ServiceInstances } from 'src/infrastructure/database/entities/ServiceInstances';
 import { AclService } from '../acl/acl.service';
 import { Test, TestingModule } from '@nestjs/testing';
-import { TestDBProviders } from 'src/infrastructure/test-utils/providers';
 import { UserService } from '../user/user.service';
+import { TestDatabaseModule } from 'src/infrastructure/database/test-database.module';
+import { TestDataService } from 'src/infrastructure/database/test-data.service';
 
 
 describe('AbilityFactory', () => {
   let abilityFactory : AbilityFactory;
   let userService : UserService; 
+  let testDataService : TestDataService; 
 
   beforeAll( async ()=>{
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TestDatabaseModule
+      ],
       providers: [
         AclService,
         AbilityFactory,
         UserService,
-        TestDBProviders.aclProvider,
-        TestDBProviders.userProvider,
+        TestDataService
       ],
     }).compile();
 
     abilityFactory = module.get<AbilityFactory>(AbilityFactory);
     userService = module.get<UserService>(UserService); 
+    testDataService = module.get<TestDataService>(TestDataService);
+
+    await testDataService.seedTestData(); 
 
   })
 
@@ -122,28 +129,28 @@ describe('AbilityFactory', () => {
       acl1.model = 'ServiceInstances';
       acl1.accessType = Action.Create;
       acl1.principalType = 'userId';
-      acl1.principalId = '1';
+      acl1.principalId = '597';
       acl1.property = 'index';
       acl1.permission = 'can';
 
       acl2.model = 'ServiceInstances';
       acl2.accessType = Action.Create;
       acl2.principalType = 'userId';
-      acl2.principalId = '2';
+      acl2.principalId = '518';
       acl2.property = 'index';
       acl2.permission = 'cannot';
 
       acl3.model = 'Invoices';
       acl3.accessType = Action.Create;
       acl3.principalType = 'userId';
-      acl3.principalId = '1';
+      acl3.principalId = '597';
       acl3.property = 'index';
       acl3.permission = 'can';
 
       acl4.model = 'Invoices';
       acl4.accessType = Action.Create;
       acl4.principalType = 'userId';
-      acl4.principalId = '2'
+      acl4.principalId = '518'
       acl4.property = 'index';
       acl4.permission = 'cannot';
 
@@ -178,17 +185,17 @@ describe('AbilityFactory', () => {
       expect(ability.can(Action.Create,instance, 'index')).toBeFalsy();
     })
 
-    it('should be true', ()=>{
+    it('should be true', async ()=>{
       const invoice = new Invoices();
-      const currentUser = new User();
-      invoice.userId = 1;
+      const currentUser = await userService.findById(518);
+      invoice.user = currentUser; 
       expect(ability.can(Action.Create,invoice, 'index')).toBeTruthy();
     })
 
-    it('should be false', ()=>{
+    it('should be false', async ()=>{
       const invoice = new Invoices();
-      const currentUser = new User();
-      invoice.userId = 2;
+      const currentUser = await userService.findById(518); 
+      invoice.user = currentUser;
       expect(ability.can(Action.Create,invoice, 'index')).toBeFalsy();
     })
 
@@ -219,7 +226,7 @@ describe('AbilityFactory', () => {
       const user = await userService.findById(597);
       const ability = await abilityFactory.createForUser(user);
       const invoice = new Invoices();
-      invoice.userId = 597;
+      invoice.user = user; 
       expect(ability.can(Action.Create,invoice,'index')).toBeTruthy(); 
     })
 
