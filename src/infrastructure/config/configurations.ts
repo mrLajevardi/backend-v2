370ce4b519b15ed8b7bc230b9e94d6e91f24e726
Majid Ziaei <ziaee.majid@gmail.com>
2023-05-30 20:13:40 +0330

    resolving config.js problem

--- src/infrastructure/config/configurations.ts
+++ src/infrastructure/config/configurations.ts
@@ -10,8 +10,6 @@ import * as path from 'path';
 const JSON_CONFIG_FILENAME = 'config.json';
 
 export default () => {
-  const p = path.join(__dirname,".");
-  console.log(p);
   const filePath = path.join(__dirname,  JSON_CONFIG_FILENAME);
   const fileContents = fs.readFileSync(filePath, 'utf8');
   const config = JSON.parse(fileContents);
