9cf4d0e5bf1c079585e2f8e5740566e528ad79a9
majid ziaei <ziaee.majid@gmail.com>
2023-06-02 19:17:15 +0330

    adding entity dtos

---
+++ src/infrastructure/dto/update/update-ai-transactions-logs.dto.ts
@@ -0,0 +1,49 @@
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateAiTransactionsLogsDto {
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  request: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  body: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  response: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  method: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  codeStatus: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  methodName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty()
+  ip: string;
+
+  @IsOptional()
+  @IsDate()
+  @ApiProperty()
+  dateTime?: Date;
+}
---
+++ src/infrastructure/dto/update/update-service-instances.dto.ts
@@ -0,0 +1,75 @@
+import { IsBoolean, IsDate, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateServiceInstanceDto {
+  @IsString()
+  @ApiProperty()
+  id: string;
+
+  @IsNumber()
+  @ApiProperty()
+  userId: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty()
+  status?: number;
+
+  @IsDate()
+  @ApiProperty()
+  updateDate: Date;
+
+  @IsDate()
+  @ApiProperty()
+  lastUpdateDate: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiProperty()
+  expireDate?: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiProperty()
+  deletedDate?: Date;
+
+  @IsOptional()
+  @IsBoolean()
+  @ApiProperty()
+  isDeleted?: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty()
+  index?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty()
+  warningSent?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty()
+  isDisabled?: number;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty()
+  name?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @ApiProperty()
+  planRatio?: number;
+
+  @IsOptional()
+  @IsDate()
+  @ApiProperty()
+  lastPayg?: Date;
+
+  @IsOptional()
+  @IsDate()
+  @ApiProperty()
+  nextPayg?: Date;
+}
