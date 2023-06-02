d044fc2f77d26dc367ec820b5a1a9bd1fabe6115
majid ziaei <ziaee.majid@gmail.com>
2023-06-02 18:07:36 +0330

    bugfix

--- src/application/base/service-instances/service-instances.module.ts
+++ src/application/base/service-instances/service-instances.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ServiceInstancesService } from './service-instances.service';
 import { ServiceInstancesController } from './service-instances.controller';
+import { DatabaseModule } from 'src/infrastructure/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [ServiceInstancesService],
   controllers: [ServiceInstancesController]
 })
