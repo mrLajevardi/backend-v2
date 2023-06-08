cbe0d0d3fb2b786763042eecb7813447487fb49f
Majid Ziaei <ziaee.majid@gmail.com>
2023-06-08 11:38:36 +0330

    bugfix

--- src/application/core/core-invoice/core-invoice.module.ts
+++ src/application/core/core-invoice/core-invoice.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/infrastructure/database/database.module';
-import { InvoiceModule } from './invoice/invoice.module';
-import { InvoicePropertiesModule } from './invoice-properties/invoice-properties.module';
 
 @Module({
-  imports: [DatabaseModule, InvoiceModule, InvoicePropertiesModule],
+  imports: [DatabaseModule],
 })
 export class CoreInvoiceModule {}
