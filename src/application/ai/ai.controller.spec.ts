0504873fb652dc354164899ff652e50d2926a292
Majid Ziaei <ziaee.majid@gmail.com>
2023-06-09 19:53:06 +0330

    fixing test dependencies

--- src/application/ai/ai.controller.spec.ts
+++ src/application/ai/ai.controller.spec.ts
@@ -3,6 +3,10 @@ import { AiController } from './ai.controller';
 import { TestDatabaseModule } from 'src/infrastructure/database/test-database.module';
 import { AiService } from './ai.service';
 import { UserService } from '../base/user/user.service';
+import { ServicePropertiesService } from '../base/service-properties/service-properties.service';
+import { ServiceInstancesService } from '../base/service-instances/service-instances.service';
+import { AiTransactionsLogsService } from '../base/ai-transactions-logs/ai-transactions-logs.service';
+import { SettingService } from '../base/setting/setting.service';
 
 describe('AiController', () => {
   let controller: AiController;
@@ -10,7 +14,14 @@ describe('AiController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestDatabaseModule],
-      providers: [AiService, UserService],
+      providers: [
+        AiService, 
+        UserService,
+        ServicePropertiesService,
+        ServiceInstancesService,
+        AiTransactionsLogsService,
+        SettingService
+      ],
       controllers: [AiController],
     }).compile();
 
--- src/application/ai/ai.service.spec.ts
+++ src/application/ai/ai.service.spec.ts
@@ -2,6 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AiService } from './ai.service';
 import { TestDatabaseModule } from 'src/infrastructure/database/test-database.module';
 import { UserService } from '../base/user/user.service';
+import { ServicePropertiesService } from '../base/service-properties/service-properties.service';
+import { ServiceInstancesService } from '../base/service-instances/service-instances.service';
+import { AiTransactionsLogsService } from '../base/ai-transactions-logs/ai-transactions-logs.service';
+import { SettingService } from '../base/setting/setting.service';
 
 describe('AiService', () => {
   let service: AiService;
@@ -9,7 +13,14 @@ describe('AiService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [TestDatabaseModule],
-      providers: [AiService, UserService],
+      providers: [
+        AiService, 
+        UserService,
+        ServicePropertiesService,
+        ServiceInstancesService,
+        AiTransactionsLogsService,
+        SettingService
+      ],
     }).compile();
 
     service = module.get<AiService>(AiService);
