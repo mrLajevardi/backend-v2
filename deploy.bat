e8550a895cb16cd0f9e6c6bee898089d2f78ffdb
Ali Lajevardi <lajevardi321@gmail.com>
2023-06-25 14:12:13 +0330

    fix ci/cd

--- deploy.bat
+++ deploy.bat
@@ -2,7 +2,4 @@ npm run pm2:stop
 del /q /s "C:\arad-v2"
 FOR /D %%p IN ("C:\arad-v2\*") DO rmdir "%%p" /s /q
 xcopy /s .\ C:\arad-v2 /Y
-cd C:\arad-v2
-npm install
-npm run build
-npm run pm2:reload
+cd C:\arad-v2&&echo "ts"&&npm install&&npm run build&&npm run pm2:reload
