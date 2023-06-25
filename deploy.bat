4d7a23596fc6066b79b19de25463f1b3d9a40798
Ali Lajevardi <lajevardi321@gmail.com>
2023-06-25 13:35:18 +0330

    added project to pm2 and added pm2 stop and reload to deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,8 @@
+pm2 stop arad-v2
 del /q /s "C:\arad-v2"
 FOR /D %%p IN ("C:\arad-v2\*") DO rmdir "%%p" /s /q
 xcopy /s .\ C:\arad-v2 /Y
 cd C:\arad-v2
 npm install
 npm run build
-@REM npm run pm2:reload
+npm run pm2:reload
