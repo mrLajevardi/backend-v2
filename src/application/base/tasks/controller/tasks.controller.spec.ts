1b8cb92a9571e21d5a799b9fd5476fd30ef14d37
Ali Lajevardi <lajevardi321@gmail.com>
2023-08-01 18:09:33 +0330

    fix test

--- src/application/base/tasks/controller/tasks.controller.spec.ts
+++ src/application/base/tasks/controller/tasks.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import { TasksService } from '../service/tasks.service';
+import { TestDatabaseModule } from 'src/infrastructure/database/test-database.module';
 
 describe('TasksController', () => {
   let controller: TasksController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [TestDatabaseModule],
       controllers: [TasksController],
     }).compile();
 
