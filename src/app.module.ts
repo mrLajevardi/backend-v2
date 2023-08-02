76db15455788e66c253c75cde1486c6264bee9d4
Majid Ziaei <ziaee.majid@gmail.com>
2023-08-02 19:56:48 +0330

    app revert back

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,6 +45,7 @@ import { GroupModule } from './application/base/group/group.module';
       },
     }),
     //infrastructure
+    DatabaseModule,
     CrudModule,
     SessionsModule,
     OrganizationModule,
@@ -74,6 +75,12 @@ import { GroupModule } from './application/base/group/group.module';
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    NetworkService,
+    ApplicationPortProfileService,
   ],
   exports: [],
 })
