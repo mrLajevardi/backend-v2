26718e717da67ce876c5ea18ab09dc3d07b2c622
Majid Ziaei <ziaee.majid@gmail.com>
2023-08-04 12:49:35 +0330

    test fix

--- src/application/base/tasks/controller/task-admin.controller.spec.ts
+++ src/application/base/tasks/controller/task-admin.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskAdminController } from './task-admin.controller';
+import { DatabaseModule } from 'src/infrastructure/database/database.module';
+import { CrudModule } from '../../crud/crud.module';
+import { TaskAdminService } from '../service/task-admin.service';
+import { ServicePropertiesModule } from '../../service-properties/service-properties.module';
+import { SessionsModule } from '../../sessions/sessions.module';
 
 describe('TaskAdminController', () => {
   let controller: TaskAdminController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        DatabaseModule,
+        CrudModule,
+        ServicePropertiesModule,
+        SessionsModule
+      ],
       controllers: [TaskAdminController],
+      providers: [TaskAdminService]
     }).compile();
 
     controller = module.get<TaskAdminController>(TaskAdminController);
--- src/application/base/tasks/service/task-admin.service.spec.ts
+++ src/application/base/tasks/service/task-admin.service.spec.ts
@@ -1,11 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TaskAdminService } from './task-admin.service';
+import { DatabaseModule } from 'src/infrastructure/database/database.module';
+import { CrudModule } from '../../crud/crud.module';
+import { ServicePropertiesModule } from '../../service-properties/service-properties.module';
+import { SessionsModule } from '../../sessions/sessions.module';
 
 describe('TaskAdminService', () => {
   let service: TaskAdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        DatabaseModule,
+        CrudModule,
+        ServicePropertiesModule,
+        SessionsModule      ],
       providers: [TaskAdminService],
     }).compile();
 
