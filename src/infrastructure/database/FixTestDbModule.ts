7ebb3fc0d12f30d837d45d7ff7f61b35dfcaa9d5
Majid Ziaei <ziaee.majid@gmail.com>
2023-08-08 13:15:11 +0330

    lint, servar max up

--- src/infrastructure/database/FixTestDbModule.ts
+++ src/infrastructure/database/FixTestDbModule.ts
@@ -4,27 +4,25 @@ Importing this module in the app.module.ts is sufficient for loading the databse
 
 */
 
-import { Module, OnModuleInit } from "@nestjs/common";
-import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { ConfigModule } from "@nestjs/config";
-import { Acl } from "./entities/Acl";
+import { Module, OnModuleInit } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Acl } from './entities/Acl';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(
-        {
-          isGlobal: true,
-          type: "sqlite",
-          database: ":memory:",
-          autoLoadEntities: true,
-          entities: [Acl],
-          synchronize: true,
-        } as TypeOrmModuleOptions,
-    ),
-   TypeOrmModule.forFeature([Acl]),
+    TypeOrmModule.forRoot({
+      isGlobal: true,
+      type: 'sqlite',
+      database: ':memory:',
+      autoLoadEntities: true,
+      entities: [Acl],
+      synchronize: true,
+    } as TypeOrmModuleOptions),
+    TypeOrmModule.forFeature([Acl]),
   ],
   exports: [TypeOrmModule],
 })
