1bc14f712d5862cd9194d2f6d78ee11e5ee60694
m.zare <mojizb7848@gmail.com>
2023-09-21 11:47:02 +0330

    edit regex

--- src/application/base/datacenter/service/datacenter.service.ts
+++ src/application/base/datacenter/service/datacenter.service.ts
@@ -29,7 +29,7 @@ export class DatacenterService implements IDatacenterService, BaseService {
     const option: FindManyOptions<ItemTypesConfig> =
       this.datacenterServiceFactory.GetFindOptionBy(query);
 
-    const regexPatternGeneration = /^G\d+$/;
+    const regexPatternGeneration = /^g\d+$/;
 
     const configs: ItemTypesConfig[] = await this.dataCenterTableService.find(
       option,
