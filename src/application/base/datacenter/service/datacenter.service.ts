1b6f47714d16f9f7bde41197f29295659fb7b613
m.zare <mojizb7848@gmail.com>
2023-09-21 14:06:34 +0330

    resolving Error for search configs

--- src/application/base/datacenter/service/datacenter.service.ts
+++ src/application/base/datacenter/service/datacenter.service.ts
@@ -94,7 +94,7 @@ export class DatacenterService implements BaseService {
     const option: FindManyOptions<ItemTypesConfig> =
       this.datacenterServiceFactory.GetFindOptionBy(query);
 
-    const regexPatternGeneration = /^g\d+$/;
+    const regexPatternGeneration = /^g[1-4]$/i;
 
     const configs: ItemTypesConfig[] = await this.dataCenterTableService.find(
       option,
