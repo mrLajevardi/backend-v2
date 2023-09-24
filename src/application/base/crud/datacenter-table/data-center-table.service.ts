f1fafb0c64909ae4172e6f904e2800cfd2df37cd
m.zare <mojizb7848@gmail.com>
2023-09-24 12:40:07 +0330

    Removing ItemTypesConfig From Code And Database also Changing ItemTypes in Code and Bank

--- src/application/base/crud/datacenter-table/data-center-table.service.ts
+++ src/application/base/crud/datacenter-table/data-center-table.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FindManyOptions, FindOneOptions, Repository } from 'typeorm';
-import { ItemTypesConfig } from '../../../../infrastructure/database/entities/ItemTypesConfig';
+// import { ItemTypesConfig } from '../../../../infrastructure/database/entities/ItemTypesConfig';
 import { IDatacenterTableService } from './IDatacenter-table.service';
 import { BaseService } from '../../../../infrastructure/service/BaseService';
+import { ItemTypes } from '../../../../infrastructure/database/entities/ItemTypes';
 
 @Injectable()
 // implements IDatacenterTableService, BaseService
@@ -11,21 +12,17 @@ export class DataCenterTableService
   implements IDatacenterTableService, BaseService
 {
   constructor(
-    @InjectRepository(ItemTypesConfig)
-    private readonly repository: Repository<ItemTypesConfig>,
+    @InjectRepository(ItemTypes)
+    private readonly repository: Repository<ItemTypes>,
   ) {}
 
-  async find(
-    option: FindManyOptions<ItemTypesConfig>,
-  ): Promise<ItemTypesConfig[]> {
-    const res: ItemTypesConfig[] = await this.repository.find(option);
+  async find(option: FindManyOptions<ItemTypes>): Promise<ItemTypes[]> {
+    const res: ItemTypes[] = await this.repository.find(option);
     return Promise.resolve(res);
   }
 
-  async findOne(
-    option: FindOneOptions<ItemTypesConfig>,
-  ): Promise<ItemTypesConfig> {
-    const res: ItemTypesConfig = await this.repository.findOne(option);
+  async findOne(option: FindOneOptions<ItemTypes>): Promise<ItemTypes> {
+    const res: ItemTypes = await this.repository.findOne(option);
     return Promise.resolve(res);
   }
 }
