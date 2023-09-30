36bc0a8d4178e65371956f9a55ff599840379698
m.zare <mojizb7848@gmail.com>
2023-09-30 04:47:59 -0700

    Mock detail vdc

--- src/application/vdc/controller/vdc.controller.ts
+++ src/application/vdc/controller/vdc.controller.ts
@@ -220,4 +220,34 @@ export class VdcController {
       data,
     );
   }
+
+  @Get('invoice/:invoiceId/details')
+  @ApiParam({ name: 'serviceInstanceId', description: 'VDC instance ID' })
+  async getVdcInvoiceDetail(
+    @Param('serviceInstanceId')
+    serviceInstanceId: string,
+  ) {
+    return {
+      datacenter: { name: 'amin', title: 'امین' },
+      cpu: { price: 1000, title: '', unit: 'Core', quantity: 10 },
+      vm: { price: 1000, title: '', unit: 'Server', quantity: 5 },
+      ram: { price: 1000, title: '', unit: 'GB', quantity: 10 },
+      disk: [
+        {
+          price: 25000,
+          title: 'archive',
+          unit: 'GB',
+          quantity: 10,
+        },
+        { price: 20000, title: 'standard', unit: 'GB', quantity: 20 },
+      ],
+      guaranty: 'VIP',
+      period: 150,
+      ip: { price: 20000, title: '', unit: 'IP', quantity: 14 },
+      generation: 'G2',
+      finalPrice: 10000,
+      reservationRam: 25,
+      reservationCpu: 25,
+    };
+  }
 }
