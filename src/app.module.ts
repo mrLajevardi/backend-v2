24791c5966082c128d54430c0261811aadeb564a
m.zare <mojizb7848@gmail.com>
2023-10-10 13:52:02 +0330

    resolve error for app module lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,7 +43,7 @@ import { TaskManagerModule } from './application/base/task-manager/task-manager.
 import { BullModule as BullMQModule } from '@nestjs/bullmq';
 import { UvdeskWrapperModule } from './wrappers/uvdesk-wrapper/uvdesk-wrapper.module';
 import { DatacenterModule } from './application/base/datacenter/datacenter.module';
-import {ServiceItemModule} from "./application/base/service-item/service-item.module";
+import { ServiceItemModule } from './application/base/service-item/service-item.module';
 
 @Module({
   imports: [
