044a3ee6c711668c458c1bbf7c04458ad1ee3289
Ali Lajevardi <lajevardi321@gmail.com>
2023-10-22 21:54:58 +0330

    fixed bugs in deploy.sh and added port to .env file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 npm run pm2:stop
-path = "/var/services/arad-v2/*"
+path="/var/services/arad-v2/*"
 rm -rf $path
-wd = pwd
-path = "/var/services/backend-v2/"
+wd=pwd
+path="/var/services/backend-v2/"
 cp -rf ./* $path
 cp /var/services/configs/.env $path
 cd $path && npm install && npm run build && npm run pm2:reload  
