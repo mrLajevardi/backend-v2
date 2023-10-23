c2f4a15c0791964a69d5f8b50f49bf1d43872bc3
Ali Lajevardi <lajevardi321@gmail.com>
2023-10-23 11:41:45 +0330

    fixed bugs

---
+++ ecosystem.config.bak.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'arad-v2.back',
+      script: './dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      node_args: 'cross-env PORT=3007 --max-http-header-size=80000',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
