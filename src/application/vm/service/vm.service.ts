2510bf73d290e59e40fa8c25c53b3185d4fc20ed
m.zare <mojizb7848@gmail.com>
2023-11-14 11:52:37 +0330

    resolving error for statuses

--- src/application/vm/service/vm.service.ts
+++ src/application/vm/service/vm.service.ts
@@ -481,7 +481,7 @@ export class VmService {
         numCoresPerSocket,
         sockets: cpu / numCoresPerSocket,
         totalStorageAllocatedMb,
-        status,
+        status: VmStatusEnum[status],
         dateCreated,
         autoDeleteDate,
         isExpired,
