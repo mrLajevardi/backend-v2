182bac8144126c088a0cfd5bb638ba55018e0c88
m.zare <mojizb7848@gmail.com>
2023-11-30 16:33:24 +0330

    resolve error  test

--- src/application/vdc/controller/vdc.controller.spec.ts
+++ src/application/vdc/controller/vdc.controller.spec.ts
@@ -26,6 +26,7 @@ import { EdgeGatewayModule } from 'src/application/edge-gateway/edge-gateway.mod
 import { VmModule } from 'src/application/vm/vm.module';
 import { NetworksModule } from 'src/application/networks/networks.module';
 import { DatacenterModule } from 'src/application/base/datacenter/datacenter.module';
+import { ServiceService } from '../../base/service/services/service.service';
 
 describe('VdcController', () => {
   let controller: VdcController;
@@ -37,7 +38,6 @@ describe('VdcController', () => {
         DatabaseModule,
         CrudModule,
         LoggerModule,
-        //TasksModule,
         SessionsModule,
         DatacenterModule,
         OrganizationModule,
@@ -58,6 +58,7 @@ describe('VdcController', () => {
         NetworkService,
         VdcFactoryService,
         VdcDetailFactoryService,
+        ServiceService,
         {
           provide: BASE_VDC_INVOICE_SERVICE,
           useClass: VdcInvoiceService,
