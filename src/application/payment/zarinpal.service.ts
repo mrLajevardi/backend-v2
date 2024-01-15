13506cea4ae98e483dbeee1cada912c8c4640ac0
m.zare <mojizb7848@gmail.com>
2024-01-15 13:58:27 +0330

    resolve error zarin pal

--- src/application/payment/zarinpal.service.ts
+++ src/application/payment/zarinpal.service.ts
@@ -19,28 +19,18 @@ export class ZarinpalService {
     let metaData: string;
     await axios
       .post('https://api.zarinpal.com/pg/v4/payment/verify.json', data)
-      .then(async (res) => {
-        await axios
-          .post('https://api.zarinpal.com/pg/v4/payment/verify.json', data)
-          .then((res) => {
-            console.log('before if res of zarinpal ==>', res);
-            if (
-              res.data.data.message === 'Verified' &&
-              (res.data.data.code === 100 || res.data.data.code === 101)
-            ) {
-              console.log('after if res of zarinpal ==>', res);
-              verified = true;
-              refID = res.data.data.ref_id;
-              metaData = res.data.data;
-            } else verified = false;
-          })
-          .catch(
-            (err) => {
-              console.log('zarinpal error ==>', err);
-              verified = false;
-            },
-            // err.response.data.errors.message,
-          );
+      .then((res) => {
+        console.log('before if res of zarinpal ==>', res);
+        if (
+          (res.data.data.message === 'Verified' ||
+            res.data.data.message === 'Paid') &&
+          (res.data.data.code === 100 || res.data.data.code === 101)
+        ) {
+          console.log('after if res of zarinpal ==>', res);
+          verified = true;
+          refID = res.data.data.ref_id;
+          metaData = res.data.data;
+        } else verified = false;
       })
       .catch(
         (err) => {
