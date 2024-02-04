ba0742be68a83fd8d64dc2915d39fcb729ad69ea
m.zare <mojizb7848@gmail.com>
2024-02-04 15:44:50 +0330

    editing schema part 1

--- src/infrastructure/database/entities/views/v-serviceInstanceDetail.ts
+++ src/infrastructure/database/entities/views/v-serviceInstanceDetail.ts
@@ -14,6 +14,7 @@ import { VServiceInstances } from './v-serviceInstances';
 
 @Entity({
   name: 'V_ServiceDetail',
+  schema: 'services',
 })
 export class VServiceInstanceDetail {
   @Column(isTestingEnv() ? 'text' : 'uniqueidentifier', {
