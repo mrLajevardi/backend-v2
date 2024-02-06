6ea7f70f1c7918943b9174ecd71d407eb8af5ee3
m.zare <mojizb7848@gmail.com>
2024-02-06 12:53:35 +0330

    resolve error for invoice

--- src/application/base/service/services/service-checks.service.ts
+++ src/application/base/service/services/service-checks.service.ts
@@ -3,14 +3,14 @@ import { InvalidServiceParamsException } from 'src/infrastructure/exceptions/inv
 import { isEmpty, isNil } from 'lodash';
 import { ErrorDetail } from '../dto/return/error-detail.dto';
 import { ServiceItemsSum } from 'src/infrastructure/database/entities/views/service-items-sum';
-import { ServiceItemsSumService } from '../../crud/service-items-sum/service-items-sum.service';
+
 import { ItemTypes } from 'src/infrastructure/database/entities/ItemTypes';
 import { ServicePaymentsTableService } from '../../crud/service-payments-table/service-payments-table.service';
 
 @Injectable()
 export class ServiceChecksService {
   constructor(
-    private readonly serviceItemsSumTable: ServiceItemsSumService,
+    // private readonly serviceItemsSumTable: ServiceItemsSumService,
     private readonly servicePaymentsTableService: ServicePaymentsTableService,
   ) {}
 
@@ -42,68 +42,68 @@ export class ServiceChecksService {
   }
 
   // Moved from service checks
-  async checkServiceItems(
-    data: [number],
-    items: ItemTypes[],
-  ): Promise<ErrorDetail | null> {
-    const itemsList = [];
-    const errorDetail = {
-      codes: {},
-    };
-    for (const item of Object.keys(items)) {
-      itemsList.push(items[item].title);
-    }
-    let status = 0;
-    for (const item of itemsList) {
-      if (!data[item] || typeof data[item] !== 'number') {
-        // MUST BE REVIEWd
-        errorDetail.codes[item] = [
-          new InvalidServiceParamsException('absense'),
-        ];
-        status = 1;
-      }
-    }
-    for (const item of itemsList) {
-      const itemSum: ServiceItemsSum = await this.serviceItemsSumTable.findById(
-        item,
-      );
-      const sum = !isNil(itemSum) ? itemSum.sum + data[item] : data[item];
-      if (
-        sum > items[itemsList.indexOf(item)].maxAvailable &&
-        items[itemsList.indexOf(item)].maxAvailable !== 0
-      ) {
-        if (!errorDetail.codes[item]) {
-          errorDetail.codes[item] = [
-            new InvalidServiceParamsException('max_available'),
-          ];
-        } else {
-          errorDetail.codes[item].push(
-            new InvalidServiceParamsException('max_per_request'),
-          );
-        }
-        status = 1;
-      }
-      // checks maxPerRequest
-      if (data[item] > items[itemsList.indexOf(item)].maxPerRequest) {
-        if (!errorDetail.codes[item]) {
-          // if errorDetails.codes[item] does not exist
-          errorDetail.codes[item] = [
-            new InvalidServiceParamsException('max_per_request'),
-          ];
-        } else {
-          // if errorDetails.codes[item] does exist
-          errorDetail.codes[item].push(
-            new InvalidServiceParamsException('max_per_request'),
-          );
-        }
-        status = 1;
-      }
-    }
-    if (status) {
-      return errorDetail;
-    }
-    return null;
-  }
+  // async checkServiceItems(
+  //   data: [number],
+  //   items: ItemTypes[],
+  // ): Promise<ErrorDetail | null> {
+  //   const itemsList = [];
+  //   const errorDetail = {
+  //     codes: {},
+  //   };
+  //   for (const item of Object.keys(items)) {
+  //     itemsList.push(items[item].title);
+  //   }
+  //   let status = 0;
+  //   for (const item of itemsList) {
+  //     if (!data[item] || typeof data[item] !== 'number') {
+  //       // MUST BE REVIEWd
+  //       errorDetail.codes[item] = [
+  //         new InvalidServiceParamsException('absense'),
+  //       ];
+  //       status = 1;
+  //     }
+  //   }
+  //   for (const item of itemsList) {
+  //     const itemSum: ServiceItemsSum = await this.serviceItemsSumTable.findById(
+  //       item,
+  //     );
+  //     const sum = !isNil(itemSum) ? itemSum.sum + data[item] : data[item];
+  //     if (
+  //       sum > items[itemsList.indexOf(item)].maxAvailable &&
+  //       items[itemsList.indexOf(item)].maxAvailable !== 0
+  //     ) {
+  //       if (!errorDetail.codes[item]) {
+  //         errorDetail.codes[item] = [
+  //           new InvalidServiceParamsException('max_available'),
+  //         ];
+  //       } else {
+  //         errorDetail.codes[item].push(
+  //           new InvalidServiceParamsException('max_per_request'),
+  //         );
+  //       }
+  //       status = 1;
+  //     }
+  //     // checks maxPerRequest
+  //     if (data[item] > items[itemsList.indexOf(item)].maxPerRequest) {
+  //       if (!errorDetail.codes[item]) {
+  //         // if errorDetails.codes[item] does not exist
+  //         errorDetail.codes[item] = [
+  //           new InvalidServiceParamsException('max_per_request'),
+  //         ];
+  //       } else {
+  //         // if errorDetails.codes[item] does exist
+  //         errorDetail.codes[item].push(
+  //           new InvalidServiceParamsException('max_per_request'),
+  //         );
+  //       }
+  //       status = 1;
+  //     }
+  //   }
+  //   if (status) {
+  //     return errorDetail;
+  //   }
+  //   return null;
+  // }
 
   async getServiceCreditBy(serviceInstanceId: string): Promise<number> {
     const servicePayment = await this.servicePaymentsTableService
