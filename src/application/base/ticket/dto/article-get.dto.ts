5ecfa05edd5913f7ca5e3087b663ff2e35f84c6f
m.zare <mojizb7848@gmail.com>
2024-02-18 14:16:13 +0330

    resolve error for get ticket

---
+++ src/application/base/ticket/dto/article-get.dto.ts
@@ -0,0 +1,10 @@
+import { ArticleListDto } from './article-list.dto';
+import { TicketTopics } from '../../crud/tickets-table/enum/ticket-topics.enum';
+
+export class ArticleGetDto {
+  topic?: TicketTopics;
+  ticket_code?: number;
+  group?: string;
+  articles?: any[];
+  state?: string;
+}
